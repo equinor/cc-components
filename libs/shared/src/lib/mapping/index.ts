5524ee25238cf447533d441022b70823075962e2
Joachim <joachim290@outlook.com>
2022-11-07 10:20:29 +0100

    feat: wip procosys urls const

--- libs/shared/src/lib/mapping/index.ts
+++ libs/shared/src/lib/mapping/index.ts
@@ -2,3 +2,4 @@ export { colorMap } from './colorMap';
 export { materialStatusMap } from './materialStatusMap';
 export { statusColorMap } from './statusColorMap';
 export { itemContentColors } from './itemContentColors';
+export { proCoSysUrls } from './procosysUrls';
