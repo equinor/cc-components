02b7610a0772bbfcde6aab822c8085c098b87bda
Joachim <joachim290@outlook.com>
2022-11-07 09:49:48 +0100

    feat: add sidesheet table component

---
+++ libs/shared/src/lib/components/table/TabTable/index.ts
@@ -0,0 +1,1 @@
+export { TabTable } from './TabTable';
---
+++ libs/shared/src/lib/components/table/cells/ProgressCell.tsx
@@ -0,0 +1,17 @@
+import {
+  StyledActualProgress,
+  StyledProgressBarContainer,
+  StyledProgressNumber,
+} from './cell.styles';
+
+type ProgressCellProps = {
+  percentWidth: number;
+};
+export const ProgressCell = ({ percentWidth }: ProgressCellProps): JSX.Element => {
+  return (
+    <StyledProgressBarContainer>
+      <StyledActualProgress borderColor="#40D38F" width={percentWidth} color="#D9F6E9" />
+      <StyledProgressNumber>{`${Math.round(percentWidth)}%`}</StyledProgressNumber>
+    </StyledProgressBarContainer>
+  );
+};
