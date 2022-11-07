7f4211054129de48c8663e2feaba15ab664e1857
Joachim <joachim290@outlook.com>
2022-11-07 11:22:43 +0100

    feat: add unsigned task sidesheet table

---
+++ libs/shared/src/lib/components/sidesheet/tabs/unsignedTask/columns.tsx
@@ -0,0 +1,25 @@
+import { proCoSysUrls } from '@cc-components/shared';
+import { ColDef, ICellRendererProps } from '@equinor/workspace-ag-grid';
+import { DescriptionCell, LinkCell } from '../../../table';
+import { UnsignedTaskBase } from './types';
+
+export const columns: ColDef<UnsignedTaskBase>[] = [
+  {
+    field: '#',
+    valueGetter: (pkg) => pkg.data?.taskNumber,
+    valueFormatter: (pkg) =>
+      pkg.data?.taskId ? proCoSysUrls.getFormTypeUrl(pkg.data.taskId) : '',
+    cellRenderer: (props: ICellRendererProps<UnsignedTaskBase>) => {
+      if (props.valueFormatted) {
+        return <LinkCell url={props.valueFormatted} urlText={props.value} />;
+      } else return null;
+    },
+  },
+  {
+    field: 'Title',
+    valueGetter: (pkg) => pkg.data?.title,
+    cellRenderer: (props: ICellRendererProps<UnsignedTaskBase, string | null>) => {
+      return <DescriptionCell description={props.value} />;
+    },
+  },
+];
