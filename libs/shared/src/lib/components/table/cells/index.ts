ef7c05a9679a02dedde9274a259d3d394f7bcc07
Joachim <joachim290@outlook.com>
2022-11-07 09:50:33 +0100

    feat: add table cell components

---
+++ libs/shared/src/lib/components/table/cells/index.ts
@@ -0,0 +1,4 @@
+export { DescriptionCell } from './DescriptionCell';
+export { DateCell } from './DateCell';
+export { ProgressCell } from './ProgressCell';
+export { EstimateCell } from './EstimateCell';
