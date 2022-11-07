0174aa6bfef48d5a87d7d23417f8551fd78845a9
Joachim <joachim290@outlook.com>
2022-11-07 10:55:17 +0100

    chore: update type for rfo and rfc

--- libs/shared/src/lib/utils-statuses/getRFCCStatus.ts
+++ libs/shared/src/lib/utils-statuses/getRFCCStatus.ts
@@ -1,9 +1,9 @@
 import { PackageStatus } from '../types';
 
 type RFCCBase = {
-  rfccIsAccepted: boolean;
-  rfccIsRejected: boolean;
-  rfccIsShipped: boolean;
+  rfccIsAccepted: boolean | null;
+  rfccIsRejected: boolean | null;
+  rfccIsShipped: boolean | null;
 };
 export const getRFCCStatus = <T extends RFCCBase>(item: T): PackageStatus =>
   item.rfccIsAccepted
