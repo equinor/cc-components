27169e8d9f03657d24788d933294a23eb9fb3e5c
Joachim <joachim290@outlook.com>
2022-11-15 15:13:49 +0100

    chore: wip

--- libs/workorderapp/src/lib/config/workspace.ts
+++ libs/workorderapp/src/lib/config/workspace.ts
@@ -5,19 +5,30 @@ import { filterConfig } from './filterConfig';
 import { gardenConfig } from './gardenConfig';
 import { statusBarConfig } from './statusBarConfig';
 import { tableConfig } from './tableConfig';
+import { IHttpClient } from '@equinor/fusion-framework-module-http';
 
-export const Workspace = createFusionWorkspace<WorkOrder>(
-  { appKey: 'Workorder', getIdentifier: (item) => item.workOrderNumber },
-  (config) =>
-    config
-      .addMiddleware((mediator) => (mediator.dataService.data = workorders))
-      .addConfig({
-        appColor: 'purple',
-        appKey: 'Workorder',
-        defaultTab: 'garden',
-      })
-      .addGrid(tableConfig)
-      .addFilter(filterConfig)
-      .addGarden(gardenConfig)
-      .addStatusBarItems(statusBarConfig)
-);
+export const Workspace = (httpClient: IHttpClient) => {
+  return createFusionWorkspace<WorkOrder>(
+    { appKey: 'Workorder', getIdentifier: (item) => item.workOrderNumber },
+    (config) =>
+      config
+        .addDataSource({
+          getResponseAsync: async () => {
+            const wos = await httpClient.fetch(
+              '/api/contexts/94dd5f4d-17f1-4312-bf75-ad75f4d9572c/work-orders'
+            );
+            console.log(wos);
+            return wos;
+          },
+        })
+        .addConfig({
+          appColor: 'purple',
+          appKey: 'Workorder',
+          defaultTab: 'grid',
+        })
+        .addGrid(tableConfig)
+        .addFilter(filterConfig)
+        .addGarden(gardenConfig)
+        .addStatusBarItems(statusBarConfig)
+  );
+};
