c156b9c2be6d5f349d19a5ea4cf24224da2d2221
Joachim <joachim290@outlook.com>
2022-11-15 15:12:29 +0100

    chore: wip

--- apps/workorder/src/app/app.tsx
+++ apps/workorder/src/app/app.tsx
@@ -1,14 +1,48 @@
 import { AppModuleInitiator } from '@equinor/fusion-framework-app';
-import { createComponent, renderComponent } from '@equinor/fusion-framework-react-app';
+import {
+  createComponent,
+  renderComponent,
+  useAppModule,
+  useAppModules,
+} from '@equinor/fusion-framework-react-app';
 import { module as serviceModule } from '@equinor/fusion-framework-module-services';
+import { Workspace } from '@cc-components/workorderapp';
+import { useHttpClient } from '@equinor/fusion-framework-react-app/http';
+
+const dataProxy = {
+  clientId: '5a842df8-3238-415d-b168-9f16a6a6031b',
+  uri: 'https://pro-s-dataproxy-CI.azurewebsites.net',
+  defaultScopes: ['5a842df8-3238-415d-b168-9f16a6a6031b/.default'],
+};
 const configure: AppModuleInitiator = async (config) => {
-  config.useFrameworkServiceClient('portal');
-  config.addConfig({
-    module: serviceModule,
+  config.configureHttpClient('data-proxy', {
+    baseUri: dataProxy.uri,
+    defaultScopes: dataProxy.defaultScopes,
   });
+  // config.configureHttpClient('FAM', {
+  //   baseUri: 'https://famapi.equinor.com',
+  //   defaultScopes: [
+  //     'api://922fbdee-795d-4733-b4ab-e1cc8bf85e8c/default',
+  //     'api://958bef40-48c3-496e-bc0b-0fe5783f196b/access_as_user',
+  //   ],
+  // });
+  // config.onInitialized((f) => console.log(f));
+  // config.useFrameworkServiceClient('portal');
+  // config.addConfig({
+  //   module: serviceModule,
+  // });
 };
 export const render = renderComponent((el, args) => {
-  const componentRenderer = createComponent(() => <div></div>, configure);
+  const componentRenderer = createComponent(() => {
+    const a = useHttpClient('data-proxy');
+    const WorkspaceComponent = Workspace(a);
+
+    return (
+      <div style={{ height: '90vh', width: '90vw' }}>
+        <WorkspaceComponent />
+      </div>
+    );
+  }, configure);
 
   return componentRenderer(el, args);
 });
---
+++ apps/workorder/vite.config.ts
@@ -0,0 +1,9 @@
+import * as path from 'path';
+export default {
+  resolve: {
+    alias: {
+      '@cc-components/workorderapp': path.resolve('../../libs/workorderapp/src'),
+      '@cc-components/shared': path.resolve('../../libs/shared/src'),
+    },
+  },
+};
