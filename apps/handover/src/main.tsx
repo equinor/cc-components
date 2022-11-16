8676f7525691c60bf043619a20ba3a9c6c2c60b5
Joachim <joachim290@outlook.com>
2022-11-16 08:17:50 +0100

    feat: fusion framework handover app

--- apps/handover/src/main.tsx
+++ apps/handover/src/main.tsx
@@ -1,11 +1,31 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
+import { createWorkspace } from '@cc-components/handoverapp';
+import { createComponent, renderComponent } from '@equinor/fusion-framework-react-app';
+import { useHttpClient } from '@equinor/fusion-framework-react-app/http';
+import { useMemo } from 'react';
+const dataProxy = {
+  clientId: '5a842df8-3238-415d-b168-9f16a6a6031b',
+  uri: 'https://pro-s-dataproxy-CI.azurewebsites.net',
+  defaultScopes: ['5a842df8-3238-415d-b168-9f16a6a6031b/.default'],
+};
+const configure = async (config: any) => {
+  config.configureHttpClient('data-proxy', {
+    baseUri: dataProxy.uri,
+    defaultScopes: dataProxy.defaultScopes,
+  });
+};
+export const render = renderComponent((el, args) => {
+  const componentRenderer = createComponent(() => {
+    const dataProxy = useHttpClient('data-proxy');
+    const Workspace = useMemo(() => createWorkspace(dataProxy), [dataProxy]);
 
-import App from './app/app';
+    return (
+      <div style={{ height: '90vh', width: '90vw' }}>
+        <Workspace />
+      </div>
+    );
+  }, configure);
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+  return componentRenderer(el, args);
+});
+
+export default render;
---
+++ apps/handover/vite.config.ts
@@ -0,0 +1,9 @@
+import * as path from 'path';
+export default {
+  resolve: {
+    alias: {
+      '@cc-components/handoverapp': path.resolve('../../libs/handoverapp/src'),
+      '@cc-components/shared': path.resolve('../../libs/shared/src'),
+    },
+  },
+};
