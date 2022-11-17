7dd938d423fe29147bd009dc4cb7c74995ba4cba
Joachim <joachim290@outlook.com>
2022-11-17 08:39:25 +0100

    chore: upgrade to new version of fusion frameworks

--- apps/handover/src/main.tsx
+++ apps/handover/src/main.tsx
@@ -1,31 +1,52 @@
 import { createWorkspace } from '@cc-components/handoverapp';
-import { createComponent, renderComponent } from '@equinor/fusion-framework-react-app';
+import {
+  ComponentRenderArgs,
+  IAppConfigurator,
+  makeComponent,
+} from '@equinor/fusion-framework-react-app';
 import { useHttpClient } from '@equinor/fusion-framework-react-app/http';
-import { useMemo } from 'react';
+import { enableAgGrid } from '@equinor/fusion-framework-module-ag-grid';
+import { StrictMode, useMemo } from 'react';
+import { createRoot } from 'react-dom/client';
 const dataProxy = {
   clientId: '5a842df8-3238-415d-b168-9f16a6a6031b',
   uri: 'https://pro-s-dataproxy-CI.azurewebsites.net',
   defaultScopes: ['5a842df8-3238-415d-b168-9f16a6a6031b/.default'],
 };
-const configure = async (config: any) => {
+
+const configure = async (config: IAppConfigurator) => {
   config.configureHttpClient('data-proxy', {
     baseUri: dataProxy.uri,
     defaultScopes: dataProxy.defaultScopes,
   });
+  enableAgGrid(config);
 };
-export const render = renderComponent((el, args) => {
-  const componentRenderer = createComponent(() => {
-    const dataProxy = useHttpClient('data-proxy');
-    const Workspace = useMemo(() => createWorkspace(dataProxy), [dataProxy]);
 
-    return (
+const MyApp = () => {
+  const dataProxy = useHttpClient('data-proxy');
+  const Workspace = useMemo(() => createWorkspace(dataProxy), [dataProxy]);
+  return (
+    <StrictMode>
       <div style={{ height: '90vh', width: '90vw' }}>
         <Workspace />
       </div>
-    );
-  }, configure);
+    </StrictMode>
+  );
+};
 
-  return componentRenderer(el, args);
-});
+export default function render(el: HTMLElement, args: ComponentRenderArgs) {
+  /** Create root from provided element */
+  const root = createRoot(el);
 
-export default render;
+  /** Make the app component
+   * First argument is the main React component
+   * Second argu is the the render args (framework and env variables)
+   * Third argument is the configuration callback
+   */
+  const AppComponent = makeComponent(<MyApp />, args, configure);
+
+  root.render(<AppComponent />);
+
+  /** Teardown */
+  return () => root.unmount();
+}
