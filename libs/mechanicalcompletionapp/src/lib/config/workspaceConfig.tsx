98b316b59d8b267adfe589d2a52c04b5f503872e
Joachim <joachim290@outlook.com>
2022-11-18 13:34:18 +0100

    chore: update to new workspace config

--- libs/mechanicalcompletionapp/src/lib/config/workspaceConfig.tsx
+++ libs/mechanicalcompletionapp/src/lib/config/workspaceConfig.tsx
@@ -1,40 +1,46 @@
-import { createFusionWorkspace } from '@equinor/workspace-fusion';
-import { IHttpClient } from '@equinor/fusion-framework-react-app/http';
+import Workspace from '@equinor/workspace-fusion';
+import { useHttpClient } from '@equinor/fusion-framework-react-app/http';
 import { McPackage } from '../types';
 import { sortPackagesByStatus } from '../utils-statuses/sortPackagesByStatus';
 import { filterConfig } from './filterConfig';
 import { statusBarConfig } from './statusBarConfig';
 import { tableConfig } from './tableConfig';
 import { gardenConfig } from './gardenConfig';
+import { useCallback } from 'react';
 
-export const createWorkspace = (httpClient: IHttpClient) => {
-  return createFusionWorkspace<McPackage>(
-    { appKey: 'MC', getIdentifier: (item) => item.mcPkgId },
-    (config) =>
-      config
-        .addConfig({
-          appColor: 'purple',
-          appKey: 'MC',
-          defaultTab: 'grid',
-        })
-        .addDataSource({
-          getResponseAsync: async () => {
-            const mcpkgs = await httpClient.fetch(
-              `/api/contexts/94dd5f4d-17f1-4312-bf75-ad75f4d9572c/mc-pkgs`
-            );
-            return mcpkgs;
-          },
-          responseParser: async (response: Response) => {
-            const parsedResponse = JSON.parse(await response.text()) as McPackage[];
-            return parsedResponse.sort(sortPackagesByStatus);
-          },
-        })
-        .addGrid(tableConfig)
-        .addFilter(filterConfig)
-        .addGarden(gardenConfig)
-        .addStatusBarItems(statusBarConfig)
-        .addFusionPowerBI({
-          reportUri: 'pp-mc-analytics',
-        })
+const useContextId = () => {
+  return '2d489afd-d3ec-43f8-b7ca-cf2de5f39a89';
+};
+export const WorkspaceWrapper = () => {
+  const dataProxy = useHttpClient('data-proxy');
+  const contextId = useContextId();
+  const getResponseAsync = useCallback(async () => {
+    const mcpkgs = await dataProxy.fetch(`/api/contexts/${contextId}/mc-pkgs`);
+    return mcpkgs;
+  }, [dataProxy, contextId]);
+
+  const responseParser = async (response: Response) => {
+    const parsedResponse = JSON.parse(await response.text()) as McPackage[];
+    return parsedResponse.sort(sortPackagesByStatus);
+  };
+
+  return (
+    <Workspace
+      workspaceOptions={{
+        appKey: 'MC',
+        getIdentifier: (item) => item.mcPkgId,
+      }}
+      filterOptions={filterConfig}
+      gardenOptions={gardenConfig}
+      gridOptions={tableConfig}
+      statusBarOptions={statusBarConfig}
+      fusionPowerBiOptions={{
+        reportUri: 'pp-mc-analytics',
+      }}
+      dataOptions={{
+        getResponseAsync,
+        responseParser,
+      }}
+    />
   );
 };
