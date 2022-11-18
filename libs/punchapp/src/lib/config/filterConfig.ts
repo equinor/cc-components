0949d75419d257bf2b88e883dea42243f11aa5cc
Joachim <joachim290@outlook.com>
2022-11-18 15:02:56 +0100

    chore: import type

--- libs/punchapp/src/lib/config/filterConfig.ts
+++ libs/punchapp/src/lib/config/filterConfig.ts
@@ -1,4 +1,4 @@
-import { FilterConfig } from '@equinor/workspace-fusion/filter';
+import type { FilterConfig } from '@equinor/workspace-fusion/filter';
 import { Punch } from '../types';
 
 export const filterConfig: FilterConfig<Punch> = {
