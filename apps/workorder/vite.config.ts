326c7fa64db8d9c9183b4bcb0bd1f736f1581e46
Joachim <joachim290@outlook.com>
2022-11-18 14:05:14 +0100

    feat: update build for apps

--- apps/workorder/vite.config.ts
+++ apps/workorder/vite.config.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 export default {
   resolve: {
     alias: {
-      '@cc-components/loopapp': path.resolve('../../libs/loopapp/src'),
+      '@cc-components/workorderapp': path.resolve('../../libs/workorderapp/src'),
       '@cc-components/shared': path.resolve('../../libs/shared/src'),
     },
   },
