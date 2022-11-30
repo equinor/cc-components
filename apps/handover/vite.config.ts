e4e055828de65980b23911b40dbf49589e52ab9e
Joachim <joachim290@outlook.com>
2022-11-30 14:52:17 +0100

    chore: add env to vite config

--- apps/handover/vite.config.ts
+++ apps/handover/vite.config.ts
@@ -1,5 +1,7 @@
 import * as path from 'path';
+import EnvironmentPlugin from 'vite-plugin-environment';
 export default {
+  plugins: [EnvironmentPlugin({ NODE_ENV: 'development' })],
   resolve: {
     alias: {
       '@cc-components/handoverapp': path.resolve('../../libs/handoverapp/src'),
