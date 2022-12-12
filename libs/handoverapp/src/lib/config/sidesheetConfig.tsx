387f6f8c6d675df1ca98b3effee4f9d27708ff26
Joachim <joachim290@outlook.com>
2022-12-12 14:32:27 +0100

    chore: update fusion and workspace

--- libs/handoverapp/src/lib/config/sidesheetConfig.tsx
+++ libs/handoverapp/src/lib/config/sidesheetConfig.tsx
@@ -3,7 +3,8 @@ import { HandoverSidesheet } from '@cc-components/handoversidesheet';
 import { SidesheetConfig } from '@equinor/workspace-fusion/sidesheet';
 
 export const sidesheetConfig: SidesheetConfig<HandoverPackage> = {
-  Sidesheet: (props) => (
+  type: 'simple',
+  DetailsSidesheet: (props) => (
     <HandoverSidesheet.Component
       id={props.id}
       item={props.item}
