db87cedf2b52eab85feacc4ac658429bf39873d8
Joachim <joachim290@outlook.com>
2023-01-30 18:59:32 +0100

    fix: add tsconfig base path

---
+++ libs/plugins/src/generators/app-library/updateBaseTsconfig.ts
@@ -0,0 +1,25 @@
+import { getWorkspaceLayout, joinPathFragments, Tree, updateJson } from '@nrwl/devkit';
+import { NormalizedSchema } from './schema';
+
+const getRootTsConfigPathInTree = (tree: Tree): string | null => {
+  for (const path of ['tsconfig.base.json', 'tsconfig.json']) {
+    if (tree.exists(path)) {
+      return path;
+    }
+  }
+};
+
+export const updateBaseTsConfig = (host: Tree, options: NormalizedSchema) => {
+  updateJson(host, getRootTsConfigPathInTree(host), (json) => {
+    const c = json.compilerOptions;
+    c.paths = c.paths || {};
+    delete c.paths[options.name];
+
+    const { libsDir } = getWorkspaceLayout(host);
+    c.paths[options.importPath] = [
+      joinPathFragments(libsDir, `${options.projectDirectory}/src/index.ts`),
+    ];
+
+    return json;
+  });
+};
