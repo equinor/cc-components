c358c5f5b8602a8f42c9df3cc075b908fbcf757c
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2023-02-01 13:54:26 +0100

    feat: :sparkles: expand powerbi utils (#114)

--- libs/shared/src/lib/types/index.ts
+++ libs/shared/src/lib/types/index.ts
@@ -3,3 +3,4 @@ export * from './pcsFollowUpStatus';
 export * from './pcsStatus';
 export { type PackageStatus } from './packageStatus';
 export { type BaseStatus } from './baseStatus';
+export * from './powerbi';
