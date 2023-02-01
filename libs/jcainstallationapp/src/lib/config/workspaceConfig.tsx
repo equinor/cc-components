d0f8a3f822ecf90696b3e42ecc85bd6659d17831
Jone Ueland <73601851+JoneUE@users.noreply.github.com>
2023-02-01 14:05:03 +0100

    feat: :sparkles: Added JCA installation and changed Punch PBI id

---
+++ libs/jcainstallationapp/src/lib/config/workspaceConfig.tsx
@@ -0,0 +1,23 @@
+import Workspace from '@equinor/workspace-fusion';
+import { usePBIOptions } from '@cc-components/shared';
+
+type WorkspaceWrapperProps = {
+  contextId: string;
+};
+
+export const WorkspaceWrapper = ({ contextId }: WorkspaceWrapperProps) => {
+  const pbi = usePBIOptions('pp-installation', {
+    column: 'ProjectName',
+    table: 'Dim_ProjectMaster',
+  });
+
+  return (
+    <Workspace
+      workspaceOptions={{
+        appKey: 'jcainstallationapp',
+        getIdentifier: () => '',
+      }}
+      powerBiOptions={pbi}
+    />
+  );
+};
