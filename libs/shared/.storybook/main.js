ea4bfb677fa03463534a1ea311aa777f2ee84b1a
Joachim <joachim290@outlook.com>
2023-02-01 15:43:11 +0100

    feat: first version of storybook

---
+++ libs/shared/.storybook/main.js
@@ -0,0 +1,24 @@
+const rootMain = require('../../../.storybook/main');
+
+const { mergeConfig } = require('vite');
+const viteTsConfigPaths = require('vite-tsconfig-paths').default;
+
+module.exports = {
+  ...rootMain,
+  core: { ...rootMain.core, builder: '@storybook/builder-vite' },
+  stories: [
+    ...rootMain.stories,
+    '../src/lib/**/*.stories.mdx',
+    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [...rootMain.addons],
+  async viteFinal(config, { configType }) {
+    return mergeConfig(config, {
+      plugins: [
+        viteTsConfigPaths({
+          root: '../../../',
+        }),
+      ],
+    });
+  },
+};
---
+++ libs/shared/src/lib/stories/LinkComponent.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { StyledItemLink } from '../components';
+export default {
+  title: 'Link component',
+  component: StyledItemLink,
+} as ComponentMeta<typeof StyledItemLink>;
+
+const Template: ComponentStory<typeof StyledItemLink> = (args) => (
+  <StyledItemLink {...args}>A link</StyledItemLink>
+);
+
+export const Link = Template.bind({});
+
+Link.args = {
+  href: 'https://www.example.com',
+  target: '_blank',
+};
