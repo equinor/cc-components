d4291ccfb629fb6d27474eee8adc8752b482769f
Jone Ueland <73601851+JoneUE@users.noreply.github.com>
2023-02-07 16:18:12 +0100

    docs: :memo: Added comment for non use of projectMaster and installed npm

--- libs/jcaspoolsapp/src/lib/config/workspaceConfig.tsx
+++ libs/jcaspoolsapp/src/lib/config/workspaceConfig.tsx
@@ -7,6 +7,7 @@ type WorkspaceWrapperProps = {
 
 export const WorkspaceWrapper = ({ contextId }: WorkspaceWrapperProps) => {
   const pbi = usePBIOptions('pp-spools-analytics', {
+    //Dim_ProjectMaster does not exist in Report. data will show up for all projects if app is visible for project and user have access
     column: 'ProjectName',
     table: 'Dim_ProjectMaster',
   });
