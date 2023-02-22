2c25fdfbf22ed0d89a3e9a8ce8d7e7d06a53c480
Joachim <joachim290@gmail.com>
2023-02-22 08:59:55 +0100

    [apps/jcainstallation] - chore: test new rollup inject plugin (#152)

--- apps/jcainstallation/CHANGELOG.md
+++ apps/jcainstallation/CHANGELOG.md
@@ -1,3 +1,6 @@
+## Version 0.0.4
+Test new rollup inject plugin
+
 ## Version 0.0.3
 Bump fusion-framework packages
 
--- apps/jcainstallation/vite.config.ts
+++ apps/jcainstallation/vite.config.ts
@@ -1,13 +1,22 @@
 import * as path from 'path';
-import EnvironmentPlugin from 'vite-plugin-environment';
+import injectProcessEnv from 'rollup-plugin-inject-process-env';
+
 export default {
-  plugins: [EnvironmentPlugin({ NODE_ENV: 'development' })],
   resolve: {
     alias: {
       '@cc-components/jcainstallationapp': path.resolve(
         '../../libs/jcainstallationapp/src'
       ),
       '@cc-components/shared': path.resolve('../../libs/shared/src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        injectProcessEnv({
+          NODE_ENV: 'development',
+        }),
+      ],
     },
   },
 };
