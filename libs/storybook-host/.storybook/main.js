7d9d0ef7d9d426f99c4e9a030f07a6be2583bba9
Joachim <joachim290@gmail.com>
2023-03-02 21:31:11 +0100

    Update main.js

--- libs/storybook-host/.storybook/main.js
+++ libs/storybook-host/.storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
   stories: ['../../**/stories/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials', 'storybook-addon-swc'],
   async viteFinal(config, { configType }) {
+    config.base = process.env.BASE_PATH || config.base;
     return mergeConfig(config, {
       plugins: [
         viteTsConfigPaths({
