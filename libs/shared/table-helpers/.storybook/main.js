bca0a9bcc586db428dd6a410aa56b118782e3c8b
Joachim <joachim290@outlook.com>
2023-03-02 13:40:26 +0100

    chore: storybook refactor

--- libs/shared/table-helpers/.storybook/main.js
+++ libs/shared/table-helpers/.storybook/main.js
@@ -3,16 +3,15 @@ const viteTsConfigPaths = require('vite-tsconfig-paths').default;
 
 module.exports = {
   core: { builder: '@storybook/builder-vite' },
-  stories: ['../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/lib/stories/*.stories.mdx',
+    '../src/lib/stories/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: ['@storybook/addon-essentials', 'storybook-addon-swc'],
   async viteFinal(config, { configType }) {
     config.base = process.env.BASE_PATH || config.base;
     return mergeConfig(config, {
-      plugins: [
-        viteTsConfigPaths({
-          root: '../../../',
-        }),
-      ],
+      plugins: [viteTsConfigPaths()],
     });
   },
 };
--- libs/storybook-host/.storybook/main.js
+++ libs/storybook-host/.storybook/main.js
@@ -2,13 +2,9 @@ const { mergeConfig } = require('vite');
 const viteTsConfigPaths = require('vite-tsconfig-paths').default;
 module.exports = {
   core: { builder: '@storybook/builder-vite' },
-  stories: [
-    '../src/lib/stories/*.stories.mdx',
-    '../src/lib/stories/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: ['@storybook/addon-essentials', 'storybook-addon-swc'],
+  stories: ['../../**/stories/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials'],
   async viteFinal(config, { configType }) {
-    config.base = process.env.BASE_PATH || config.base;
     return mergeConfig(config, {
       plugins: [viteTsConfigPaths()],
     });
