87882fb19197134fc5dcd8848a9c6f381e128fbe
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2023-04-26 09:06:13 +0200

    Feat/no access message (#275)
    * chore: release
    
    * feat:✨no access message data proxy
    
    ---------
    
    Co-authored-by: MagnusGjosund <maggu@live.no>

--- libs/shared/src/packages/data-proxy/src/components/UnauthorizedPage.tsx
+++ libs/shared/src/packages/data-proxy/src/components/UnauthorizedPage.tsx
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 import { FusionDataProxyError } from '../types/fusionDataProxyError';
+import { error_outlined } from '@equinor/eds-icons';
+import { Icon, Typography } from '@equinor/eds-core-react';
+import { tokens } from '@equinor/eds-tokens';
+
+Icon.add({ error_outlined });
 
 type UnauthorizedErrorProps = {
   error: FusionDataProxyError;
@@ -7,20 +12,35 @@ type UnauthorizedErrorProps = {
 export function UnathorizedPage({ error }: UnauthorizedErrorProps) {
   return (
     <StyledFusionError>
-      <StyledErrorTitle>{error.error.message}</StyledErrorTitle>
+      <Icon
+        name={error_outlined.name}
+        size={48}
+        color={tokens.colors.interactive.warning__resting.hex}
+      />
+      <Typography
+        variant="h1"
+        group="heading"
+        color={tokens.colors.interactive.warning__resting.hex}
+        bold
+        lines={1}
+      >
+        Looks like you do not have access to the selected content
+      </Typography>
       <div>
-        <div>Requirements:</div>
-        {error.error.accessRequirements.map((s) => (
-          <div key={s.code}>{s.code}</div>
-        ))}
+        <Typography>
+          Access is only given to members with one of the following roles:
+        </Typography>
+        <StyledList>
+          {error.error.accessRequirements.map((s) => (
+            <li title={s.description} key={s.code}>
+              <Typography>{s.code}</Typography>
+            </li>
+          ))}
+        </StyledList>
       </div>
     </StyledFusionError>
   );
 }
-
-const StyledErrorTitle = styled.h1`
-  max-width: 500px;
-`;
 
 const StyledFusionError = styled.div`
   height: 100%;
@@ -29,4 +49,9 @@ const StyledFusionError = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 2em;
+`;
+
+const StyledList = styled.ul`
+  padding-inline-start: 1em;
 `;
