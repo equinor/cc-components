dfd9c655a8eb43e380762f61875b53830f8682eb
MagnusGjosund <maggu@live.no>
2023-04-27 09:54:28 +0200

    chore: remove loop mock data

--- libs/loopsidesheet/src/lib/utils-sidesheet/useGetWorkorders.ts
+++ libs/loopsidesheet/src/lib/utils-sidesheet/useGetWorkorders.ts
@@ -1,10 +1,28 @@
-import { mockWorkorders } from "./mockWorkorder"
+import { useHttpClient } from '@equinor/fusion-framework-react-app/http';
+import { useContextId } from '@cc-components/shared';
+import { useQuery } from '@tanstack/react-query';
+import { Workorder } from '../types';
 
-export const useGetWorkorders = () => {
-
-    return {
-        data: mockWorkorders,
-        isLoading: false,
-        error: null
+export const useGetWorkorders = (loopNo: string) => {
+  const client = useHttpClient('cc-api');
+  const contextId = useContextId();
+  const { data, isLoading, error } = useQuery<Workorder[], Error>(
+    ['loop', loopNo, 'workorders'],
+    async ({ signal }) => {
+      const respons = await client.fetch(
+        `/api/contexts/${contextId}/loop/${loopNo}/workorders`,
+        { signal }
+      );
+      if (!respons.ok) {
+        throw new Error();
+      }
+      return respons.json();
     }
-}
+  );
+
+  return {
+    data: data,
+    isLoading: isLoading,
+    error: error,
+  };
+};
