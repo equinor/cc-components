7fe32724ec668a441e79b8f4583431b5b56f521b
Gustav Eikaas <guei@equinor.com>
2023-05-09 10:06:57 +0200

    chore: add spinners

--- cli/src/commands/download_zip_bundle.ts
+++ cli/src/commands/download_zip_bundle.ts
@@ -1,13 +1,17 @@
 import { writeFileSync } from 'fs';
 import { fetch } from 'cross-fetch';
+import ora from 'ora';
 
 export async function downloadCIBundle(appKey: string) {
+  const spinner = ora().start('Downloading bundle from CI');
   const res = await fetch(
     `https://backend-fusion-project-portal-test.radix.equinor.com/api/bundles/${appKey}`
   );
   if (!res.ok) {
+    spinner.fail('Failed to download bundle');
     throw new ApiError('Failed to fetch bundle', res);
   }
+  spinner.succeed('Downloaded bundle successfully');
   const content = await res.text();
   writeFileSync('./dist/app-bundle.js', content);
 }
