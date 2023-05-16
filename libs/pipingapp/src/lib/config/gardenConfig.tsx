666f6cc937f3608f8e9f87e2c40622ef7753d750
MagnusGjosund <maggu@live.no>
2023-05-16 14:01:26 +0200

    feat: initial commit

---
+++ libs/pipingapp/src/lib/config/gardenConfig.tsx
@@ -0,0 +1,9 @@
+import { GardenConfig } from '@equinor/workspace-fusion/garden';
+
+export const gardenConfig: GardenConfig<any> = {
+  getDisplayName: (item) => item.serialNumber,
+  initialGrouping: {
+    horizontalGroupingAccessor: 'state',
+    verticalGroupingKeys: [],
+  },
+};
