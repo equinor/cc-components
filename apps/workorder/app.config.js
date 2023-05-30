830099174216a020f9bf05b74fa862248495e851
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2023-05-30 09:07:09 +0200

    feat: :sparkles: application insights (#360)

--- apps/workorder/app.config.js
+++ apps/workorder/app.config.js
@@ -10,6 +10,7 @@ export default () => ({
   environment: {
     uri: 'https://backend-fusion-data-gateway-test.radix.equinor.com',
     defaultScopes: ['api://ed6de162-dd30-4757-95eb-0ffc8d34fbe0/access_as_user'],
+    ai: 'InstrumentationKey=b6f0c853-1b41-4277-ac0a-982005b50a2d;IngestionEndpoint=https://norwayeast-0.in.applicationinsights.azure.com/;LiveEndpoint=https://norwayeast.livediagnostics.monitor.azure.com/',
   },
   endpoints: {},
 });
