05c2bbe42ab1693bb704b2a82fabffea73c061fd
Gustav Eikaas <guei@equinor.com>
2023-06-12 08:43:01 +0200

    fix: :bug: fix ts error

--- libs/loopapp/src/lib/config/gardenConfig.ts
+++ libs/loopapp/src/lib/config/gardenConfig.ts
@@ -1,10 +1,11 @@
 import { Loop } from '@cc-components/loopshared';
 import { GardenConfig } from '@equinor/workspace-fusion/garden';
+import { FilterState } from '@equinor/workspace-fusion/filter';
 import { GardenItem } from '../ui-garden';
 import { useHttpClient } from '@equinor/fusion-framework-react-app/http';
 import { useGardenDataSource } from '@cc-components/shared/workspace-config';
 
-export const useGardenConfig = (contextId: string): GardenConfig<Loop, unknown> => {
+export const useGardenConfig = (contextId: string): GardenConfig<Loop, FilterState> => {
   const client = useHttpClient('cc-api');
   const { getBlockAsync, getGardenMeta, getHeader, getSubgroupItems } =
     useGardenDataSource({
