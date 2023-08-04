00733a3843b9dfe3dcc2145c8b1368bdb319923d
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2023-08-04 15:31:14 +0200

    chore/migrate loop workspace v3 (#439)
    * chore: :arrow_up: update loop workspace version to 3.0.0
    
    * fix: :bug: fix loading sidesheet for loop showing error while loading
    
    * chore: fix build error

--- libs/loopapp/src/lib/config/loopSidesheet.tsx
+++ libs/loopapp/src/lib/config/loopSidesheet.tsx
@@ -5,10 +5,6 @@ import { SidesheetConfig } from '@equinor/workspace-fusion/sidesheet';
 export const sidesheetConfig: SidesheetConfig<Loop> = {
   type: 'default',
   DetailsSidesheet: (props) => (
-    <LoopSidesheet.Component
-      id={props.id}
-      item={props.item}
-      close={props.controller.close}
-    />
+    <LoopSidesheet.Component id={props.id} item={props.item} close={props.close} />
   ),
 };
