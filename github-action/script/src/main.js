a117ea1d562f1f1a8bb70b26d363d7850917ff08
Gustav Eikaas <guei@equinor.com>
2023-08-10 15:45:58 +0200

    ci:

--- github-action/script/src/main.js
+++ github-action/script/src/main.js
@@ -1,6 +1,6 @@
 import { execSync } from 'child_process';
 import { join } from 'path';
-import { getInput, debug, setFailed, setOutput, setCommandEcho, notice, setSecret, } from '@actions/core';
+import { getInput, debug, setFailed, setCommandEcho, setSecret, } from '@actions/core';
 const run = async () => {
     try {
         setCommandEcho(true);
@@ -9,16 +9,16 @@ const run = async () => {
         const workingDirectory = getInput('working-directory', { required: false }) ?? './';
         const cwd = join(process.cwd(), workingDirectory);
         debug(`Running command: ${turboCommand} in directory ${workingDirectory}`);
-        const json = execSync(`npx turbo run ${turboCommand} --dry-run=json`, {
+        const json = execSync(`npx turbo run ${turboCommand}`, {
             cwd: cwd,
             encoding: 'utf-8',
         });
-        debug(`Output from Turborepo: ${json}`);
-        const parsedOutput = JSON.parse(json);
-        debug(`Packages that changed ${parsedOutput.packages.toString()}`);
-        notice(`Packages that changed ${parsedOutput.packages.toString()}`);
-        const changed = !!parsedOutput.packages.length;
-        setOutput('changed', changed);
+        // debug(`Output from Turborepo: ${json}`);
+        // const parsedOutput = JSON.parse(json);
+        // debug(`Packages that changed ${parsedOutput.packages.toString()}`);
+        // notice(`Packages that changed ${parsedOutput.packages.toString()}`);
+        // const changed = !!parsedOutput.packages.length;
+        // setOutput('changed', changed);
     }
     catch (error) {
         if (error instanceof Error || typeof error === 'string') {
