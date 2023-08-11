5e204b1f4fd0dbaad42d1786fd8ee2bed32375a4
Gustav Eikaas <guei@equinor.com>
2023-08-11 08:59:57 +0200

    ci:

--- libs/handoversidesheet/src/lib/ui-sidesheet/sidesheet.styles.ts
+++ libs/handoversidesheet/src/lib/ui-sidesheet/sidesheet.styles.ts
@@ -1,13 +1,13 @@
-import { Tabs } from '@equinor/eds-core-react';
+import { TabListProps, Tabs } from '@equinor/eds-core-react';
 import { tokens } from '@equinor/eds-tokens';
 import styled from 'styled-components';
 
-export const StyledTabListWrapper = styled.div`
+export const StyledTabListWrapper: (props: any) => JSX.Element = styled.div`
   overflow: hidden;
   width: 100%;
   background-color: ${tokens.colors.ui.background__light.hex};
 `;
-export const StyledTabsList = styled(Tabs.List)`
+export const StyledTabsList: (props: TabListProps) => JSX.Element = styled(Tabs.List)`
   overflow: auto;
   ::-webkit-scrollbar {
     width: 0;
