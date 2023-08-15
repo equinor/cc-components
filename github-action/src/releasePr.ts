0826e899701fc6f17a65d433f91017305bf0face
Gustav Eikaas <guei@equinor.com>
2023-08-15 08:25:00 +0200

    ci: test

--- github-action/src/releasePr.ts
+++ github-action/src/releasePr.ts
@@ -79,29 +79,28 @@ async function patchWithPrNumber(prNumber: string, token: string, appKey: string
   }
 }
 
-const ColorReset = '\x1b[0m';
+// const ColorReset = '\x1b[0m';
 
-enum TextEffect {
-  Bright = '\x1b[1m',
-  Dim = '\x1b[2m',
-  Underscore = '\x1b[4m',
-  Blink = '\x1b[5m',
-  Reverse = '\x1b[7m',
-  Hidden = '\x1b[8m',
-}
+// enum TextEffect {
+//   Bright = '\x1b[1m',
+//   Dim = '\x1b[2m',
+//   Underscore = '\x1b[4m',
+//   Blink = '\x1b[5m',
+//   Reverse = '\x1b[7m',
+//   Hidden = '\x1b[8m',
+// }
 
-enum ForegroundColor {
-  Black = '\x1b[30m',
-  Red = '\x1b[31m',
-  Green = '\x1b[32m',
-  Yellow = '\x1b[33m',
-  Blue = '\x1b[34m',
-  Magenta = '\x1b[35m',
-  Cyan = '\x1b[36m',
-  White = '\x1b[37m',
-}
+// enum ForegroundColor {
+//   Black = '\x1b[30m',
+//   Red = '\x1b[31m',
+//   Green = '\x1b[32m',
+//   Yellow = '\x1b[33m',
+//   Blue = '\x1b[34m',
+//   Magenta = '\x1b[35m',
+//   Cyan = '\x1b[36m',
+//   White = '\x1b[37m',
+// }
 
 export function logInfo(message: string): void {
-  const textFormat = `${TextEffect.Underscore}${ForegroundColor.Cyan}`;
-  console.log(`${textFormat}${message}${ColorReset}`);
+  console.log(`\x1b[32m${message}`);
 }
