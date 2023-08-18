03adc347c7686df7cc80ab9840ef746b59ccfe4f
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2023-08-18 11:04:08 +0200

    dx:default context selection for devs (#496)

--- libs/shared/src/packages/common/src/lib/NoContext.tsx
+++ libs/shared/src/packages/common/src/lib/NoContext.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { useModuleCurrentContext } from '@equinor/fusion-framework-react-module-context';
+import { useLayoutEffect } from 'react';
 
 const StyledNoContext = styled.div`
   display: flex;
@@ -6,6 +8,14 @@ const StyledNoContext = styled.div`
   align-items: center;
 `;
 export const NoContext = () => {
+  const { setCurrentContext } = useModuleCurrentContext();
+
+  useLayoutEffect(() => {
+    if (window.location.href.includes('localhost')) {
+      setCurrentContext('ce31b83a-b6cd-4267-89f3-db308edf721e');
+    }
+  }, [window.location.href]);
+
   return (
     <StyledNoContext>
       <h2>Choose a valid context</h2>
