4b913eb30511d5e25e40c49de4ccbe14675ce5f9
MagnusGjosund <maggu@live.no>
2023-09-07 13:41:01 +0200

    feat: :sparkles: Refactor code and remove a scrollbar

--- libs/electricalconsumersapp/src/lib/components/CircuitDiagram.tsx
+++ libs/electricalconsumersapp/src/lib/components/CircuitDiagram.tsx
@@ -9,99 +9,125 @@ Icon.add({ heat_trace, cable, junction_box, circuit });
 
 export function CircuitDiagram({ network }: { network: ElectricalNetwork }) {
   return (
-    <div
-      style={{
-        display: 'flex',
-        width: '100%',
-        overflow: 'auto',
-        fontSize: '12px',
-      }}
-    >
+    <StyledCircuitDiagram>
       <Switchboard network={network} />
-      <Wrapper
-        style={{
-          gridTemplateRows: new Array(network.children.length).fill('1fr').join(' '),
-        }}
-      >
+      <StyledSwitchboardChildren networkChildrenLength={network.children.length}>
         {network.children
           .map((s) => s.children)
           .flat()
           .map((s) => (
-            <ItemStuffThing network={s} key={s.name} />
+            <ElectricalComponent network={s} key={s.name} />
           ))}
-      </Wrapper>
-    </div>
+      </StyledSwitchboardChildren>
+    </StyledCircuitDiagram>
   );
 }
 
 function Switchboard({ network }: { network: ElectricalNetwork }) {
   return (
-    <div
-      style={{
-        background: tokens.colors.ui.background__light.hex,
-        border: `1px solid ${tokens.colors.ui.background__medium.hex}`,
-        width: '110px',
-        display: 'flex',
-        alignItems: 'flex-start',
-        flexDirection: 'column',
-        gap: '50px',
-        padding: '10px 5px 0px 5px',
-        borderRadius: '10px',
-      }}
-    >
-      <SwtichBoardItems>
+    <StyledSwitchboardWrapper>
+      <StyledNetworkNameAndIcon>
         {network.name} <SwitchBoardIcon />
-      </SwtichBoardItems>
-      <div>
-        {network.children.map((s) => (
-          <StyledCircuitItems key={s.name}>
-            {s.name} <Icon name={circuit.name} />
-            {s.isSafetyCritical ? <CriticalLineVisual /> : null}
-          </StyledCircuitItems>
-        ))}
-      </div>
-    </div>
+      </StyledNetworkNameAndIcon>
+      {network.children.map((s) => (
+        <StyledCircuitNameAndIcon key={s.name}>
+          {s.name} <Icon name={circuit.name} />
+          {s.isSafetyCritical ? <CriticalLine /> : null}
+        </StyledCircuitNameAndIcon>
+      ))}
+    </StyledSwitchboardWrapper>
   );
 }
 
-const SwtichBoardItems = styled.div`
+const StyledSwitchboardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 110px;
+  background: ${tokens.colors.ui.background__light.hex};
+  border: 1px solid ${tokens.colors.ui.background__medium.hex};
+  gap: 5ch;
+  padding: 10px 5px 0px 5px;
+  border-radius: 10px;
+`;
+
+const StyledCircuitDiagram = styled.div`
+  display: flex;
+  width: 100%;
+  overflow: auto;
+  font-size: 12px;
+  padding: 20px;
+`;
+
+const StyledCircuitNameAndIcon = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
+  padding: 6px;
+  gap: 1ch;
+  background: rgb(255, 255, 255);
+  border: 1px solid rgb(220, 220, 220);
+  border-radius: 10px;
+`;
+
+const StyledNetworkNameAndIcon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   gap: 1ch;
 `;
 
 const SwitchBoardIcon = () => {
   return (
-    <>
-      <svg
-        width="2rem"
-        height="1.8rem"
-        viewBox="0 0 56 57"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        style={{ rotate: '0deg;' }}
-      >
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M2 2.002v52.5h52.5v-52.5H2zm3.5 49v-45.5H51v45.5H5.5z"
-          fill="#243746"
-        ></path>
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M45.25 10.502h-34c-.97 0-1.75.78-1.75 1.75s.78 1.75 1.75 1.75h34c.97 0 1.75-.78 1.75-1.75s-.78-1.75-1.75-1.75zM45.25 18.502h-34c-.97 0-1.75.78-1.75 1.75s.78 1.75 1.75 1.75h34c.97 0 1.75-.78 1.75-1.75s-.78-1.75-1.75-1.75zM45.25 26.502h-34c-.97 0-1.75.78-1.75 1.75s.78 1.75 1.75 1.75h34c.97 0 1.75-.78 1.75-1.75s-.78-1.75-1.75-1.75z"
-          fill="#243746"
-        ></path>
-      </svg>
-    </>
+    <svg
+      width="2rem"
+      height="1.8rem"
+      viewBox="0 0 56 57"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ rotate: '0deg;' }}
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M2 2.002v52.5h52.5v-52.5H2zm3.5 49v-45.5H51v45.5H5.5z"
+        fill="#000"
+      ></path>
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M45.25 10.502h-34c-.97 0-1.75.78-1.75 1.75s.78 1.75 1.75 1.75h34c.97 0 1.75-.78 1.75-1.75s-.78-1.75-1.75-1.75zM45.25 18.502h-34c-.97 0-1.75.78-1.75 1.75s.78 1.75 1.75 1.75h34c.97 0 1.75-.78 1.75-1.75s-.78-1.75-1.75-1.75zM45.25 26.502h-34c-.97 0-1.75.78-1.75 1.75s.78 1.75 1.75 1.75h34c.97 0 1.75-.78 1.75-1.75s-.78-1.75-1.75-1.75z"
+        fill="#000"
+      ></path>
+    </svg>
   );
 };
-const StyledCircuitItems = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 1ch;
-`;
+
+const SpaceHeaterIcon = () => {
+  return (
+    <svg
+      width="2rem"
+      height="1.8rem"
+      viewBox="0 0 56 56"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ rotate: '0deg' }}
+    >
+      <g clip-path="url(#varme_svg__clip0_1814_4864)" stroke="#132634" stroke-width="3.3">
+        <path stroke-linecap="round" d="M1.65 28.684h52.7"></path>
+        <path fill="#ffffff" d="M8.65 17.65h38.7v20.7H8.65z"></path>
+        <path
+          stroke-linecap="round"
+          d="M18.15 17.984v20.033M27.55 17.984v20.033M37.451 17.984v20.033"
+        ></path>
+      </g>
+      <defs>
+        <clipPath id="varme_svg__clip0_1814_4864">
+          <path fill="#fff" d="M0 0h56v56H0z"></path>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
 
 const StyledHTCable = styled.div`
   display: flex;
@@ -115,7 +141,8 @@ const StyledHTCable = styled.div`
     width: 12px;
     height: 12px;
     border: 1px solid white;
-    background-color: ${(props) => 'black'};
+    background-color: ${(props) =>
+      'black'}; //mulighet for å endre farge når pipetest blir implementert
     border-radius: 50%;
     margin: 0px 1px;
     content: ' ';
@@ -127,70 +154,77 @@ const StyledHTCable = styled.div`
 
 const StyledJunctionBox = styled.div`
   box-sizing: border-box;
-  min-height: 60px;
+  min-height: 50px;
   white-space: nowrap;
   padding: 10px 5px 0px 5px;
   border: 1px solid ${tokens.colors.ui.background__medium.hex};
-  /* border: 1px solid black; */
   border-radius: 10px;
-`;
-
-const StyledJunctionBoxItems = styled.div`
-  display: flex;
-  align-items: center;
-  height: fit-content;
-  gap: 1ch;
 `;
 
 function JunctionBox({ network }: { network: ElectricalNetwork }) {
   return (
     <StyledJunctionBox>
-      <StyledJunctionBoxItems>
-        {network.name} <Icon name={junction_box.name} />
-      </StyledJunctionBoxItems>
+      <StyledNetworkNameAndIcon>
+        {network.name}
+        <Icon name={junction_box.name} />
+        {network.isSafetyCritical ? <CriticalLine /> : null}
+      </StyledNetworkNameAndIcon>
     </StyledJunctionBox>
   );
 }
 
 export const HTCable = ({ network }: { network: ElectricalNetwork }) => {
   return (
-    <>
-      <StyledHTCable>
-        {network.name} <Icon name={heat_trace.name} />{' '}
-        {network.isSafetyCritical ? <CriticalLineVisual /> : null}
-      </StyledHTCable>
-    </>
+    <StyledHTCable>
+      {network.name}
+      <Icon name={heat_trace.name} />
+      {network.isSafetyCritical ? <CriticalLine /> : null}
+    </StyledHTCable>
   );
 };
 
-function ItemStuffThing({ network }: { network: ElectricalNetwork }) {
+export const SpaceHeater = ({ network }: { network: ElectricalNetwork }) => {
+  return (
+    <StyledSpaceHeater>
+      <StyledNetworkNameAndIcon>
+        {network.name}
+        <SpaceHeaterIcon />
+        {network.isSafetyCritical ? <CriticalLine /> : null}
+      </StyledNetworkNameAndIcon>
+    </StyledSpaceHeater>
+  );
+};
+
+function Cable({ network }: { network: ElectricalNetwork }) {
+  return (
+    <StyledCable>
+      <StyledNetworkNameAndIcon>
+        {network.name}
+        <Icon name={cable.name} />
+        {network.isSafetyCritical ? <CriticalLine /> : null}
+      </StyledNetworkNameAndIcon>
+    </StyledCable>
+  );
+}
+
+function ElectricalComponent({ network }: { network: ElectricalNetwork }) {
   switch (network.eleSymbolCode) {
     case 'HT_KAB': {
       return <HTCable network={network} />;
     }
 
+    case 'VARME': {
+      return <SpaceHeater network={network} />;
+    }
+
     case 'KABEL': {
       return (
         <Item>
-          <Cable name={network.name} />
+          <Cable network={network} />
 
           <ChildWrapper>
             {network.children.map((s) => (
-              <ItemStuffThing network={s} key={s.name} />
-            ))}
-          </ChildWrapper>
-        </Item>
-      );
-    }
-
-    case 'KURS': {
-      return (
-        <Item>
-          <Name>{network.name}</Name>
-
-          <ChildWrapper>
-            {network.children.map((s) => (
-              <ItemStuffThing network={s} key={s.name} />
+              <ElectricalComponent network={s} key={s.name} />
             ))}
           </ChildWrapper>
         </Item>
@@ -204,63 +238,7 @@ function ItemStuffThing({ network }: { network: ElectricalNetwork }) {
 
           <ChildWrapper>
             {network.children.map((s) => (
-              <ItemStuffThing network={s} key={s.name} />
-            ))}
-          </ChildWrapper>
-        </Item>
-      );
-    }
-
-    case 'TAVLE': {
-      return (
-        <Item>
-          <Name>{network.name}</Name>
-
-          <ChildWrapper>
-            {network.children.map((s) => (
-              <ItemStuffThing network={s} key={s.name} />
-            ))}
-          </ChildWrapper>
-        </Item>
-      );
-    }
-
-    case 'VARME': {
-      return (
-        <Item>
-          <StyledSpaceHeater>
-            {network.name}{' '}
-            <svg
-              width="2rem"
-              height="1.8rem"
-              viewBox="0 0 56 56"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              style={{ rotate: '0deg' }}
-            >
-              <g
-                clip-path="url(#varme_svg__clip0_1814_4864)"
-                stroke="#132634"
-                stroke-width="3.3"
-              >
-                <path stroke-linecap="round" d="M1.65 28.684h52.7"></path>
-                <path fill="#ffffff" d="M8.65 17.65h38.7v20.7H8.65z"></path>
-                <path
-                  stroke-linecap="round"
-                  d="M18.15 17.984v20.033M27.55 17.984v20.033M37.451 17.984v20.033"
-                ></path>
-              </g>
-              <defs>
-                <clipPath id="varme_svg__clip0_1814_4864">
-                  <path fill="#fff" d="M0 0h56v56H0z"></path>
-                </clipPath>
-              </defs>
-            </svg>
-          </StyledSpaceHeater>
-
-          <ChildWrapper>
-            {network.children.map((s) => (
-              <ItemStuffThing network={s} key={s.name} />
+              <ElectricalComponent network={s} key={s.name} />
             ))}
           </ChildWrapper>
         </Item>
@@ -274,7 +252,7 @@ function ItemStuffThing({ network }: { network: ElectricalNetwork }) {
 
           <ChildWrapper>
             {network.children.map((s) => (
-              <ItemStuffThing network={s} key={s.name} />
+              <ElectricalComponent network={s} key={s.name} />
             ))}
           </ChildWrapper>
         </Item>
@@ -282,25 +260,14 @@ function ItemStuffThing({ network }: { network: ElectricalNetwork }) {
   }
 }
 
-function Cable({ name }: { name: string }) {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        gap: '1ch',
-        borderBottom: '2px solid black',
-        height: '5ch',
-        whiteSpace: 'nowrap',
-        padding: '0px 10px',
-      }}
-    >
-      {name} <Icon name={cable.name} />
-    </div>
-  );
-}
+const StyledCable = styled.div`
+  border-bottom: 2px solid black;
+  height: 5ch;
+  white-space: nowrap;
+  padding: 0px 10px;
+`;
 
-const CriticalLineVisual = (): JSX.Element => {
+const CriticalLine = (): JSX.Element => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const onOpen = () => setIsOpen(true);
@@ -308,29 +275,19 @@ const CriticalLineVisual = (): JSX.Element => {
 
   return (
     <>
-      <TestDotWrapper onMouseOver={onOpen} onMouseLeave={onClose}>
-        <CriticalLineVisualStyle>
-          <TestDotCircleText>CL</TestDotCircleText>
-          {isOpen && (
-            <div>
-              <CircuitDiagramPopover>{'Heating Critical Line'}</CircuitDiagramPopover>
-            </div>
-          )}
-        </CriticalLineVisualStyle>
-      </TestDotWrapper>
+      <StyledCriticalLine onMouseOver={onOpen} onMouseLeave={onClose}>
+        CL
+        {isOpen && (
+          <div>
+            <StyledPopover>Heating Critical Line</StyledPopover>
+          </div>
+        )}
+      </StyledCriticalLine>
     </>
   );
 };
 
-const TestDotWrapper = styled.span`
-  display: flex;
-  flex-direction: horizontal;
-  flex: 1;
-  justify-content: center;
-  padding-bottom: 3px;
-`;
-
-const CriticalLineVisualStyle = styled.div`
+const StyledCriticalLine = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -341,15 +298,11 @@ const CriticalLineVisualStyle = styled.div`
   background-color: ${tokens.colors.ui.background__light.hex};
   border: 1px solid;
   border-color: ${tokens.colors.interactive.danger__resting.hex};
-`;
-
-export const TestDotCircleText = styled.div`
   font-size: 16px;
-  font-weight: 400, regular;
   cursor: default;
 `;
 
-const CircuitDiagramPopover = styled.div<{ cornerButton?: boolean }>`
+const StyledPopover = styled.div<{ cornerButton?: boolean }>`
   position: absolute;
   z-index: 100;
   color: #fff;
@@ -360,9 +313,11 @@ const CircuitDiagramPopover = styled.div<{ cornerButton?: boolean }>`
   right: ${(p) => (p.cornerButton ? '0px' : null)};
 `;
 
-const Wrapper = styled.div`
+const StyledSwitchboardChildren = styled.div<{ networkChildrenLength: number }>`
   display: grid;
   gap: 2em;
+  grid-template-rows: ${({ networkChildrenLength }) =>
+    new Array(networkChildrenLength).fill('1fr').join(' ')};
 `;
 
 const Item = styled.div`
@@ -381,14 +336,12 @@ const Name = styled.div`
 `;
 
 const StyledSpaceHeater = styled.div`
-  display: flex;
-  width: fit-content;
+  box-sizing: border-box;
+  min-height: 50px;
+  white-space: nowrap;
+  padding: 10px 5px 0px 5px;
   border: 1px solid ${tokens.colors.ui.background__medium.hex};
   border-radius: 10px;
-  padding: 10px 5px 0px 5px;
-  min-height: 60px;
-  box-sizing: border-box;
-  white-space: nowrap;
 `;
 
 const ChildWrapper = styled.div`
