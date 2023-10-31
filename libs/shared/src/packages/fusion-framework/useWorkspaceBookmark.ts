34d079d1dff706200c95ea3b2365acf3e90b40e1
Gustav Eikaas <guei@equinor.com>
2023-10-31 09:39:05 +0100

    feat: :sparkles: add bookmarks to handover

---
+++ libs/shared/src/packages/fusion-framework/useWorkspaceBookmark.ts
@@ -0,0 +1,33 @@
+import { Bookmark } from '@equinor/workspace-fusion';
+import { useRef, useEffect } from 'react';
+import {
+  useCurrentBookmark,
+  useBookmark,
+} from '@equinor/fusion-framework-react-app/bookmark';
+
+export function useWorkspaceBookmarks() {
+  const bookmarkRef = useRef<Partial<Bookmark>>({});
+  const bookmark = useCurrentBookmark<Bookmark>().currentBookmark;
+  const id = bookmark?.id;
+  const payload = bookmark?.payload;
+  const bookmarkHandler = useBookmark<Partial<Bookmark>>();
+
+  useEffect(() => {
+    const unsub = bookmarkHandler.addBookmarkCreator(async () => bookmarkRef.current);
+
+    return () => {
+      typeof unsub === 'function' && unsub();
+    };
+  }, [bookmarkHandler]);
+
+  const onBookmarkChange = (ref: Partial<Bookmark>) => {
+    bookmarkRef.current = ref;
+  };
+
+  return {
+    currentBookmark: payload,
+    bookmarkKey: id,
+    /** Add as prop to workspace */
+    onBookmarkChange,
+  };
+}
