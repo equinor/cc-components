e8bc01e6071183129e50a3c73088bb6cce2d023c
idrisbo <112471345+idrisbo@users.noreply.github.com>
2023-11-01 10:22:39 +0100

    Change uri to test server Commit lock file

--- apps/scopechangerequest/app.config.js
+++ apps/scopechangerequest/app.config.js
@@ -8,7 +8,7 @@ export default () => ({
     name: name,
   },
   environment: {
-    uri: 'https://localhost:7074',
+    uri: 'https://backend-scope-change-control-test.radix.equinor.com',
     defaultScopes: ['api://4e1ab5e5-9b1a-4366-9638-6fe5624ae550/access_as_user'],
   },
   endpoints: {},
