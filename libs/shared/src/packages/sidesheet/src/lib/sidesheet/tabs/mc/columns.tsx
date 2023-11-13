65c847c04c52ad01bf7e719b6dd2d6103b9ce225
espenkalle <eskal@equinor.com>
2023-11-13 14:28:47 +0100

    fix

--- libs/shared/src/packages/sidesheet/src/lib/sidesheet/tabs/mc/columns.tsx
+++ libs/shared/src/packages/sidesheet/src/lib/sidesheet/tabs/mc/columns.tsx
@@ -57,7 +57,7 @@ export const columns: ColDef<McBase>[] = [
   },
   {
     field: 'RFCC',
-    valueGetter: (pkg) => pkg.data?.rfC_Status,
+    valueGetter: (pkg) => pkg.data?.rfcStatus,
     cellRenderer: (props: ICellRendererProps<McBase, PackageStatus | undefined>) => (
       <StatusCell
         content={`${props.value}`}
@@ -73,7 +73,7 @@ export const columns: ColDef<McBase>[] = [
   },
   {
     field: 'RFOC',
-    valueGetter: (pkg) => pkg.data?.rfO_Status,
+    valueGetter: (pkg) => pkg.data?.rfoStatus,
     cellRenderer: (props: ICellRendererProps<McBase, PackageStatus | undefined>) => (
       <StatusCell
         content={`${props.value}`}
