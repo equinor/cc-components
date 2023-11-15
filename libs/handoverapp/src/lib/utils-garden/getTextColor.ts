05427938b2766af4dfab9cd460508ea369adf77c
Espen Kalleberg <108390105+espenkalle@users.noreply.github.com>
2023-11-15 09:20:56 +0100

    fix (#682)

--- libs/handoverapp/src/lib/utils-garden/getTextColor.ts
+++ libs/handoverapp/src/lib/utils-garden/getTextColor.ts
@@ -3,7 +3,13 @@ import { tokens } from '@equinor/eds-tokens';
 
 export const getTextColor = (status: PackageStatus) => {
   return (
-    ['RFO Accepted', 'RFR Accepted', 'DCC Accepted', 'RFO Rejected'] as PackageStatus[]
+    [
+      'RFO Accepted',
+      'RFO Partly signed',
+      'RFR Accepted',
+      'DCC Accepted',
+      'RFO Rejected',
+    ] as PackageStatus[]
   ).includes(status)
     ? tokens.colors.text.static_icons__primary_white.rgba
     : tokens.colors.text.static_icons__default.rgba;
