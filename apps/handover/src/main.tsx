d5f0e0e0ff2f2a35c73d23a409c0fe9672e7d190
Gustav Eikaas <guei@equinor.com>
2023-11-15 10:37:40 +0100

    chore: remove unused dep

--- apps/handover/src/main.tsx
+++ apps/handover/src/main.tsx
@@ -1,16 +1,13 @@
 import { configure, WorkspaceWrapper } from '@cc-components/handoverapp';
 import { createRender, RootAppWrapper } from '@cc-components/shared';
 import { useHttpClient } from '@equinor/fusion-framework-react-app/http';
-import { ThemeProvider, theme } from '@equinor/fusion-react-styles';
 
 const HandoverApp = () => {
   const client = useHttpClient('cc-app');
   return (
-    <ThemeProvider theme={theme}>
-      <RootAppWrapper client={client}>
-        <WorkspaceWrapper />
-      </RootAppWrapper>
-    </ThemeProvider>
+    <RootAppWrapper client={client}>
+      <WorkspaceWrapper />
+    </RootAppWrapper>
   );
 };
 
