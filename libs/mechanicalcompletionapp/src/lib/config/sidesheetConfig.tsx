233f1001e74a60d10b9eeb67ffed0451e339f585
espenkalle <eskal@equinor.com>
2023-12-06 14:02:15 +0100

    Removed "double" sidesheet

--- libs/mechanicalcompletionapp/src/lib/config/sidesheetConfig.tsx
+++ libs/mechanicalcompletionapp/src/lib/config/sidesheetConfig.tsx
@@ -4,7 +4,5 @@ import { SidesheetConfig } from '@equinor/workspace-fusion/sidesheet';
 
 export const sidesheetConfig: SidesheetConfig<McPackage> = {
   type: 'default',
-  DetailsSidesheet: (props) => (
-    <McSideSheet.Component id={props.id} item={props.item} closeSidesheet={props.close} />
-  ),
+  DetailsSidesheet: McSideSheet,
 };
