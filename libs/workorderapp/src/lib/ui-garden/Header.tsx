6b35b0966de3edb9e5392ca862e50cdbba2e32a7
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2023-12-14 10:29:38 +0100

    feat: :sparkles: remaining hours garden header (#770)

--- libs/workorderapp/src/lib/ui-garden/Header.tsx
+++ libs/workorderapp/src/lib/ui-garden/Header.tsx
@@ -2,30 +2,34 @@ import { CustomHeaderView } from '@equinor/workspace-fusion/garden';
 import { memo } from 'react';
 import styled from 'styled-components';
 
-const HeaderContent = styled.div`
+const StyledHeaderContent = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   font-weight: 600;
 `;
-const Count = styled.span`
+
+const StyledCount = styled.h2`
   color: #000000;
   font-weight: 300;
   font-size: 0.8rem;
   margin-left: 0.8em;
 `;
+
+const StyledHeaderText = styled.h2`
+  white-space: pre-line;
+  font-size: 16px;
+`;
+
 const WorkOrderHeader = (props: CustomHeaderView) => {
   const { header } = props;
 
   return (
-    <HeaderContent>
-      <div>
-        {header.name}
-        <div>{''}</div>
-      </div>
-      <Count>({header.count})</Count>
-    </HeaderContent>
+    <StyledHeaderContent>
+      <StyledHeaderText>{header.name}</StyledHeaderText>
+      <StyledCount>({header.count})</StyledCount>
+    </StyledHeaderContent>
   );
 };
 
