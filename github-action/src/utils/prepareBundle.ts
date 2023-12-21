09ab6d799e5d39a4c83c8e015ecde62623a8384f
Gustav Eikaas <guei@equinor.com>
2023-12-21 11:48:03 +0100

    ci:

--- github-action/src/utils/prepareBundle.ts
+++ github-action/src/utils/prepareBundle.ts
@@ -4,5 +4,5 @@ import { notice } from '@actions/core';
 export function prepareBundle() {
   notice('bundling application');
   execSync('node -v', { stdio: 'inherit' });
-  execSync('pnpm vite build --logLevel silent', { stdio: 'inherit' });
+  execSync('pnpm vite build', { stdio: 'inherit' });
 }
