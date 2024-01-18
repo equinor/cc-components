388363f5a9edac94c2266cbc8408dfe16c794396
Gustav Eikaas <guei@equinor.com>
2024-01-18 08:49:46 +0100

    build: inject process

--- apps/modelviewer/vite.config.ts
+++ apps/modelviewer/vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
-import EnvironmentPlugin from 'vite-plugin-environment';
+import { InjectProcessPlugin } from '../../patches/3d-patch.ts';
 
 export default defineConfig({
-  plugins: [
-    EnvironmentPlugin({
-      NODE_ENV: 'production',
-    }),
-  ],
   appType: 'custom',
+
   build: {
     emptyOutDir: true,
+    rollupOptions: {
+      plugins: [InjectProcessPlugin],
+    },
     lib: {
       entry: './src/main.tsx',
       fileName: 'app-bundle',
