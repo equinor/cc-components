8c9929c1f222e0803a8509fdacb5b0115567d18e
Kjell Håland <kjhal@equinor.com>
2024-02-19 10:25:26 +0100

    🎁: Implemented report access-information fetcing (#869)
    * Updated @equinor/workspace-fusion to latest version
    * Added custom report-access-error component showing report info.

---
+++ libs/shared/src/packages/pbi-helpers/src/lib/error-component/AccessErrorComponent.tsx
@@ -0,0 +1,103 @@
+import {
+  Accordion,
+  Card,
+  Typography,
+  Button,
+  Divider,
+  CircularProgress,
+} from '@equinor/eds-core-react';
+import { PersonAvatar } from '@equinor/fusion-react-person';
+import Markdown from 'markdown-to-jsx';
+
+import {
+  StyledContentContainer,
+  StyledErrorContent,
+  StyledErrorLoadingWrapper,
+  StyledErrorWrapper,
+  StyledHeading,
+  StyledReportDescriptionWrapper,
+  StyledReportDetailsWrapper,
+  StyledReportOwnerWrapper,
+} from './accessErrorComponentStyles';
+
+import { useReportErrorInfo } from '../useReportErrorInfo';
+
+interface Props {
+  reportUri: string;
+  title?: string;
+  subtitle?: string;
+}
+
+export const AccessErrorComponent = (props: Props) => {
+  const {
+    reportUri,
+    title = 'Restricted Access',
+    subtitle = 'It looks like you do not have access to this report',
+  } = props;
+
+  const { data, isLoading, error } = useReportErrorInfo(reportUri);
+
+  if (isLoading) {
+    return (
+      <StyledErrorLoadingWrapper>
+        <CircularProgress />
+      </StyledErrorLoadingWrapper>
+    );
+  }
+
+  if (error || !data) {
+    return <div>Failed to load error message</div>;
+  }
+
+  return (
+    <StyledErrorWrapper>
+      <StyledErrorContent>
+        <StyledHeading>{title}</StyledHeading>
+        <StyledContentContainer>
+          <Card elevation="raised">
+            <Card.Header>
+              <Card.HeaderTitle>
+                <Typography variant="h3">{subtitle}</Typography>
+                <Markdown>{data.accessDescription}</Markdown>
+              </Card.HeaderTitle>
+              <Button variant="ghost_icon"></Button>
+            </Card.Header>
+            <Card.Content>
+              <Divider style={{ width: '100%' }} />
+              <StyledReportDetailsWrapper>
+                <StyledReportDescriptionWrapper>
+                  <Typography variant="h5">Report description</Typography>
+                  <Markdown>{data.description}</Markdown>
+                </StyledReportDescriptionWrapper>
+
+                <StyledReportOwnerWrapper>
+                  <Typography variant="h5">Report owner</Typography>
+                  <Card.Header style={{ paddingTop: '20px', paddingBottom: '20px' }}>
+                    <PersonAvatar azureId={data.ownedBy.azureUniqueId} />
+                    <Card.HeaderTitle>
+                      <Typography variant="body_short_bold">
+                        {data.ownedBy.mail}
+                      </Typography>
+                      <Typography variant="body_short">{data.ownedBy.name}</Typography>
+                    </Card.HeaderTitle>
+                  </Card.Header>
+                </StyledReportOwnerWrapper>
+              </StyledReportDetailsWrapper>
+            </Card.Content>
+          </Card>
+        </StyledContentContainer>
+
+        <StyledContentContainer>
+          <Accordion>
+            <Accordion.Item>
+              <Accordion.Header>Access control description</Accordion.Header>
+              <Accordion.Panel>
+                <Markdown>{data.errorMessage}</Markdown>
+              </Accordion.Panel>
+            </Accordion.Item>
+          </Accordion>
+        </StyledContentContainer>
+      </StyledErrorContent>
+    </StyledErrorWrapper>
+  );
+};
