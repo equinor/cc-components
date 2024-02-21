48c3300f51e69d8cd6a00cd9f6ab3a3c0a57ec76
Espen Kalleberg <108390105+espenkalle@users.noreply.github.com>
2024-02-21 09:12:55 +0100

    Chore/update ncr to use query api (#866)
    * change identifier
    
    * updated type and sidesheet for handover
    
    * mc sidesheet

--- libs/shared/src/packages/sidesheet/src/lib/sidesheet/tabs/ncr/columns.tsx
+++ libs/shared/src/packages/sidesheet/src/lib/sidesheet/tabs/ncr/columns.tsx
@@ -9,24 +9,81 @@ import {
 
 export const columns: ColDef<NcrBase>[] = [
   {
-    headerName: 'Document No.',
-    valueGetter: (pkg) => pkg.data?.documentNumber,
-    cellRenderer: (props: ICellRendererProps<NcrBase, string>) => {
-      if (props.data) {
-        //return <LinkCell url={props.data.url} urlText={props.value} />; Deadlinks
-        return <StyledMonospace>{props.data.documentNumber}</StyledMonospace>;
-      } else return null;
+    headerName: 'Title',
+    valueGetter: (pkg) => pkg.data?.title,
+    cellRenderer: (props: ICellRendererProps<NcrBase, string | null>) => {
+      return (
+        <LinkCell
+          url={props.data?.url ?? ''}
+          urlText={props.value ?? ''}
+          tooltipText="Open in Fusion Query"
+        />
+      );
+    },
+    minWidth: 250,
+    flex: 2,
+  },
+  {
+    headerName: 'Requirement Reference',
+    valueGetter: (pkg) => pkg.data?.requirementReference,
+    cellRenderer: (props: ICellRendererProps<NcrBase, string | null>) => {
+      return <DescriptionCell description={props.value} displayFullText />;
     },
     minWidth: 200,
     flex: 1,
   },
   {
-    headerName: 'Title',
-    valueGetter: (pkg) => pkg.data?.title,
+    headerName: 'State',
+    valueGetter: (pkg) => pkg.data?.state,
+    cellRenderer: (props: ICellRendererProps<NcrBase, string | null>) => {
+      return <DescriptionCell description={props.value} displayFullText />;
+    },
+    minWidth: 100,
+    flex: 1,
+  },
+  {
+    headerName: 'Description',
+    valueGetter: (pkg) => pkg.data?.description,
+    cellRenderer: (props: ICellRendererProps<NcrBase, string | null>) => {
+      return <DescriptionCell description={props.value} displayFullText />;
+    },
+    minWidth: 250,
+    flex: 2,
+  },
+  {
+    headerName: 'Proposed Solution',
+    valueGetter: (pkg) => pkg.data?.proposedSolution,
     cellRenderer: (props: ICellRendererProps<NcrBase, string | null>) => {
       return <DescriptionCell description={props.value} displayFullText />;
     },
     minWidth: 200,
-    flex: 2,
+    flex: 1,
+  },
+  {
+    headerName: 'Initiator Reference',
+    valueGetter: (pkg) => pkg.data?.initiatorReference,
+    cellRenderer: (props: ICellRendererProps<NcrBase, string | null>) => {
+      return <DescriptionCell description={props.value} displayFullText />;
+    },
+    minWidth: 200,
+    flex: 1,
+  },
+  {
+    headerName: 'Current Step',
+    valueGetter: (pkg) => pkg.data?.currentStep,
+    cellRenderer: (props: ICellRendererProps<NcrBase, string | null>) => {
+      return <DescriptionCell description={props.value} displayFullText />;
+    },
+    minWidth: 150,
+    flex: 1,
+  },
+  {
+    headerName: 'Current Step State',
+    valueGetter: (pkg) => pkg.data?.currentStepState,
+    cellRenderer: (props: ICellRendererProps<NcrBase, string | null>) => {
+      return <DescriptionCell description={props.value} displayFullText />;
+    },
+    minWidth: 150,
+    flex: 1,
   },
 ];
