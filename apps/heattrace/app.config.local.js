4c35fd0a89534bdd6607e92afebe358447a29a13
Gustav Eikaas <guei@equinor.com>
2024-04-09 13:39:32 +0200

    remove env vars from ht & pt

--- apps/heattrace/app.config.local.js
+++ apps/heattrace/app.config.local.js
@@ -9,8 +9,6 @@ export default () => ({
   },
   environment: {
     uri: 'https://localhost:7074',
-    electricalUri:
-      'https://electrical-consumers-fusion-data-gateway-test.radix.equinor.com',
     defaultScopes: ['api://ed6de162-dd30-4757-95eb-0ffc8d34fbe0/access_as_user'],
   },
   endpoints: {},
