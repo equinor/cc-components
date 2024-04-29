1eff1c15e0f8c9d07371fb813432751091e94f4c
MagnusGjosund <maggu@live.no>
2024-04-29 12:04:54 +0200

    remove unused imports

--- libs/heattracesidesheet/src/lib/ui-sidesheet/HeattraceSidesheet.tsx
+++ libs/heattracesidesheet/src/lib/ui-sidesheet/HeattraceSidesheet.tsx
@@ -30,11 +30,7 @@ import { useGetHeatTraceChecklists } from '../utils-sidesheet/useGetChecklists';
 import { useQuery } from '@tanstack/react-query';
 import { CircuitDiagramTab } from './CircuitDiagramTab';
 import { ModelViewerTab } from '@cc-components/modelviewer';
-import {
-  EchoConfig,
-  EchoTag,
-  useGetEchoConfig,
-} from '../utils-sidesheet/useGetEchoConfig';
+import { useGetEchoConfig } from '../utils-sidesheet/useGetEchoConfig';
 import { Icon, Tabs } from '@equinor/eds-core-react';
 import { useGetEleNetwork } from '../utils-sidesheet/useGetEleNetwork';
 import styled from 'styled-components';
