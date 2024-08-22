ae4b3855ab40e7f11b0777b1b1d8cbc15e12ce9d
Kjell Håland <kjhal@equinor.com>
2024-08-22 12:40:50 +0200

    🎁 Heated-tags tab in HeatTrace app (#1081)
    * Added heated-tags tab to HeatTrace app

---
+++ libs/heattraceshared/src/lib/types/heatTraceHeatedTag.ts
@@ -0,0 +1,14 @@
+export type HeatTraceHeatedTag = {
+  heatTracingCableTagNo: string;
+  heatTracingCableTagId: string;
+  heatedTagNo: string;
+  heatedTagId: string;
+  register: string;
+  description: string;
+  mechanicalCompletionHandoverStatus?: string;
+  commissioningPackageNo?: string;
+  mechanicalCompletionPackageNo?: string;
+  commissioningPackageUrl?: string;
+  mechanicalCompletionUrl?: string;
+  tagUrl?: string;
+};
