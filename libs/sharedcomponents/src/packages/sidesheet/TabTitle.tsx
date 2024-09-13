09d05e0acc96986b7ffba38a36e9d8bd236180ea
Gustav Eikaas <guei@equinor.com>
2024-09-13 08:49:14 +0200

    feat(handover): sidesheet improvements

--- libs/sharedcomponents/src/packages/sidesheet/TabTitle.tsx
+++ libs/sharedcomponents/src/packages/sidesheet/TabTitle.tsx
@@ -1,8 +1,10 @@
 import { Progress } from '@equinor/eds-core-react';
+import { StatusCircle, statusColorMap } from '@cc-components/shared'
 
 type LoadingTabTitleProps<T> = {
   isLoading: boolean;
   data: T[] | undefined;
+  completed?: number
 };
 /**
  * Standard component for displaying a loading spinner and length of dataset.
@@ -11,12 +13,27 @@ type LoadingTabTitleProps<T> = {
 export const TabTitle = <T extends Record<string, unknown>>({
   data,
   isLoading,
+  completed
 }: LoadingTabTitleProps<T>): JSX.Element => {
+  const total = data?.length ?? 0;
   if (isLoading) {
     return <Progress.Dots color="primary" />;
   }
   if (!data) {
     return <>(0)</>;
   }
-  return <>({data.length})</>;
+
+  if (total > 0 && completed !== undefined) {
+    if (completed == total) {
+      return <>
+        {" "}
+        ({total})
+        {" "}
+        <StatusCircle content={``} statusColor={statusColorMap["OK"]} />
+      </>
+    }
+    return <>({completed}/{total})</>;
+  }
+
+  return <>({total})</>;
 };
