15507923b3d9307c4839f808b7778a71facad619
Edward Brunton <42774600+EdwardBrunton@users.noreply.github.com>
2024-10-01 13:46:14 +0200

    chore: Remove unused imports in getTextColor.ts

--- libs/handoverapp/src/lib/utils-garden/getTextColor.ts
+++ libs/handoverapp/src/lib/utils-garden/getTextColor.ts
@@ -1,7 +1,5 @@
 import { PackageStatus } from '@cc-components/shared/types';
-import { tokens } from '@equinor/eds-tokens';
 import { itemContentColors } from '@cc-components/shared/mapping';
-import { it } from 'node:test';
 
 export const getTextColor = (status: PackageStatus) => {
   return (
