f408b90c0ffac8d1601cf4cbb03f20138b82a4e3
Kjell Haaland <kjell.haland@bouvet.no>
2024-10-04 08:18:20 +0200

    Added support for facility in PBI

--- libs/heattraceapp/src/lib/config/workspaceConfig.tsx
+++ libs/heattraceapp/src/lib/config/workspaceConfig.tsx
@@ -16,6 +16,18 @@ import {
 import { CCApiAccessLoading } from '@cc-components/sharedcomponents';
 import { useGardenConfig } from './gardenConfig';
 import { sidesheetConfig } from './heattraceSidesheet';
+import { useModuleCurrentContext } from '@equinor/fusion-framework-react-module-context';
+
+const pbi_context_mapping = {
+  Facility: {
+    column: 'Facility',
+    table: 'Dim_Facility',
+  },
+  ProjectMaster: {
+    column: 'ProjectMaster GUID',
+    table: 'Dim_ProjectMaster',
+  },
+} as const;
 
 export const WorkspaceWrapper = () => {
   const contextId = useContextId();
@@ -27,16 +39,18 @@ export const WorkspaceWrapper = () => {
     client.fetch(`/api/contexts/${contextId}/heat-trace/filter-model`, req)
   );
 
+  const { currentContext } = useModuleCurrentContext();
+
   const { bookmarkKey, currentBookmark, onBookmarkChange } = useWorkspaceBookmarks();
 
   const tableConfig = useTableConfig(contextId);
   const statusBarConfig = useStatusBarConfig(contextId);
   const gardenConfig = useGardenConfig(contextId);
 
-  const pbi = usePBIOptions('cc-heattrace-analytics', {
-    column: 'ProjectMaster GUID',
-    table: 'Dim_ProjectMaster',
-  });
+  const pbi = usePBIOptions(
+    'cc-heattrace-analytics',
+    pbi_context_mapping[currentContext?.type.id as 'ProjectMaster' | 'Facility']
+  );
 
   if (isLoading) {
     return <CCApiAccessLoading />;
