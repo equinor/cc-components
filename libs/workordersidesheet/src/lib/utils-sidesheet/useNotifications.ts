4c519bd40895a896608a27ba4ed3bd036223f936
Kjell Håland <kjhal@equinor.com>
2024-10-21 09:20:17 +0200

    🎁 WorkOrder Notifications (#1153)
    * Added notifications to workorder

---
+++ libs/workordersidesheet/src/lib/utils-sidesheet/useNotifications.ts
@@ -0,0 +1,23 @@
+import { useContextId, useHttpClient } from '@cc-components/shared';
+import { useQuery } from '@tanstack/react-query';
+import { WorkOrderNotification } from '../types';
+
+export const useNotifications = (workOrderId: string) => {
+  const client =  useHttpClient();
+  const contextId = useContextId();
+
+  const { data, isFetching, error } = useQuery<WorkOrderNotification[], Error>({
+    queryKey: ['workOrder', workOrderId, 'notification'],
+    queryFn: async({ signal }) => {
+        const response = await client.fetch(`/api/contexts/${contextId}/work-orders/${workOrderId}/notifications`, { signal });
+
+        if (!response.ok) {
+          throw new Error('Failed to get notifications', { cause: response });
+        }
+
+        return response.json();
+    },
+  });
+
+    return { data, isFetching, error };
+};
