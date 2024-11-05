b08705c2c7603ec75fb1fdb25072bd07a1a1d346
Gustav Eikaas <guei@equinor.com>
2024-11-05 11:21:55 +0100

    add package json

--- github-action/src/utils/zipBundle.ts
+++ github-action/src/utils/zipBundle.ts
@@ -7,12 +7,14 @@ export function zipBundle() {
   notice('zipping bundle');
   const appManifestPath = resolve('./dist/app-manifest.json');
   const bundlePath = resolve('./dist/app-bundle.js');
+  const packageJsonPath = resolve('./package.json');
 
   var zip = new AdmZip();
 
   //TODO: scan files in package.json
   zip.addLocalFile(appManifestPath);
   zip.addLocalFile(bundlePath);
+  zip.addLocalFile(packageJsonPath);
 
   zip.writeZip('./dist/bundle.zip');
   return zip;
