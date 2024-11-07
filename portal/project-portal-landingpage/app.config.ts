af6d41f243cb3375d3a0e920d1a33105d40089f4
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-11-07 15:16:50 +0100

    feat: Add project-portal-landingpage

---
+++ portal/project-portal-landingpage/app.config.ts
@@ -0,0 +1,7 @@
+import { defineAppConfig } from '@equinor/fusion-framework-cli';
+
+export default defineAppConfig(() => ({
+	environment: {
+		// some env data
+	},
+}));
---
+++ portal/project-portal-landingpage/app.manifest.config.ts
@@ -0,0 +1,5 @@
+import { defineAppManifest } from '@equinor/fusion-framework-cli';
+
+export default defineAppManifest(async (_env) => ({
+	displayName: 'Project Portal Landingpage',
+}));
