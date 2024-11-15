cb82544dacd024f02ea04d714bc0c91687fdf914
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-11-15 14:30:00 +0100

    fix: comment out PortalContextSelector in ProjectPortalPage component

--- portal/project-portal-landingpage/src/App.tsx
+++ portal/project-portal-landingpage/src/App.tsx
@@ -4,10 +4,9 @@ import { ProjectPortalPage } from './pages/ProjectPortalPage';
 const queryClient = new QueryClient();
 
 export const App = () => (
-  // <QueryClientProvider client={queryClient}>
-  //   <ProjectPortalPage />;
-  // </QueryClientProvider>
-  <h1>Project Portal - Hello World!</h1>
+  <QueryClientProvider client={queryClient}>
+    <ProjectPortalPage />;
+  </QueryClientProvider>
 );
 
 export default App;
