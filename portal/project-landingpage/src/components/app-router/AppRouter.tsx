441ea6af9b3d43c159a22f0eb22aa7930d4c79cc
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-11-18 14:57:15 +0100

    fix: enhance error handling in AppRouter by adding console logging

--- portal/project-landingpage/src/components/app-router/AppRouter.tsx
+++ portal/project-landingpage/src/components/app-router/AppRouter.tsx
@@ -3,38 +3,38 @@ import { RouteObject, RouterProvider, useRouteError } from 'react-router-dom';
 import { useNavigationModule } from '@equinor/fusion-framework-react-app/navigation';
 import { ProjectPage } from '../../pages/ProjectPage';
 
-
-
 const ErrorPage = () => {
-	const error = useRouteError() as Error;
-
-	return( <div>
-			{error && (
-		<div>
-				<h4>{error.name}</h4>
-				<p>{error.message}</p>
-			</div>
-			)}
-		</div>);
+  const error = useRouteError() as Error;
+  console.log(error);
+  return (
+    <div>
+      {error && (
+        <div>
+          <h4>{error.name}</h4>
+          <p>{error.message}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 const routes: RouteObject[] = [
-	{
-		path: '/',
-		element: <ProjectPage />,
-		children: [
-			{
-				path: ':contextId/*',
-				element: <ProjectPage />,
-				errorElement: <ErrorPage/>,
-			},
-		],
-		errorElement: <ErrorPage/>,
-	},
+  {
+    path: '/',
+    element: <ProjectPage />,
+    children: [
+      {
+        path: ':contextId/*',
+        element: <ProjectPage />,
+        errorElement: <ErrorPage />,
+      },
+    ],
+    errorElement: <ErrorPage />,
+  },
 ];
 
 export const AppRouter = () => {
-	const navigation = useNavigationModule();
-	const router = useMemo(() => navigation.createRouter(routes), []);
-	return <RouterProvider router={router} />;
+  const navigation = useNavigationModule();
+  const router = useMemo(() => navigation.createRouter(routes), []);
+  return <RouterProvider router={router} />;
 };
