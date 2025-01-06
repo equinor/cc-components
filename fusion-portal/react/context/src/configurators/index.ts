eb7507cf48f933207c575ce888c917bf129d9e4a
Christopher Berge Hove <christopher.hove@bouvet.no>
2025-01-06 10:57:51 +0100

    chore: remove portal apps

--- fusion-portal/react/context/src/configurators/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './portal-context-configurators';
-export * from './portal-context-history';
--- portal/project-portal-common/src/hooks/index.ts
+++
@@ -1,1 +0,0 @@
-export { useNavigateOnContextChange } from './use-navigate-on-context-change';
