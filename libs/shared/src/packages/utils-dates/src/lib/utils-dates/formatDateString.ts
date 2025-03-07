ef9fa75cb707d5701d5ed5cbbe72cddb8395d400
kslazykv <kslaz@equinor.com>
2025-03-07 07:54:15 +0100

    fixed formatDateString

--- libs/shared/src/packages/utils-dates/src/lib/utils-dates/formatDateString.ts
+++ libs/shared/src/packages/utils-dates/src/lib/utils-dates/formatDateString.ts
@@ -6,7 +6,6 @@
 export const formatDateString = (dateString: string | null): string => {
   if (!dateString) return 'N/A';
   const date = new Date(dateString);
-  if (date.toString() === 'Invalid Date') return 'N/A';
-  const dateParts = new Intl.DateTimeFormat(undefined).formatToParts(date);
-  return `${dateParts[0].value}/${dateParts[2].value}/${dateParts[4].value}`;
+  if (isNaN(date.getTime())) return 'N/A';
+  return new Intl.DateTimeFormat('en-GB').format(date);
 };
